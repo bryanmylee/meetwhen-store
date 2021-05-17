8e47def0ee7956a926b80e9e43f8229fe08aee11
Bryan Lee <bryanmylee@gmail.com>
2021-05-18 02:20:06 +0800

    doc: add reminder for context function

--- functions/src/security/context.ts
+++ functions/src/security/context.ts
@@ -6,6 +6,13 @@ import { firebaseAdmin } from '../firebase/setup';
 export type Principal = auth.DecodedIdToken | null;
 export type Context = ExpressContext & { principal: Principal };
 
+/**
+ * The `context` function generates the context that is exposed to Resolver methods with @Ctx. It
+ * is passed as a configuration option when initializing ApolloServer.
+ *
+ * `context` is used to verify any JWT cookies on the server request and populate the `principal`
+ * property.
+ */
 export const context: ContextFunction<ExpressContext, object> = async ({ req, res }) => {
   try {
     const principal: Principal = await firebaseAdmin.auth().verifyIdToken(req.cookies.__session);
