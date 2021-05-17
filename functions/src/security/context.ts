e5c822d4d5ac1b6b11b20a9c4b152047c5062ab1
Bryan Lee <bryanmylee@gmail.com>
2021-05-17 23:14:20 +0800

    feat: add context type

--- functions/src/security/context.ts
+++ functions/src/security/context.ts
@@ -2,10 +2,13 @@ import { ContextFunction } from 'apollo-server-core';
 import { ExpressContext } from 'apollo-server-express';
 import Container from 'typedi';
 import { TokenService } from '../token/service';
+import { Principal } from '../token/types';
+
+export type Context = ExpressContext & { principal: Principal | null };
 
 export const context: ContextFunction<ExpressContext, object> = ({ req, res }) => {
   const principal = req.cookies.__session
     ? Container.get(TokenService).verifyAccessToken(req.cookies.__session)
-    : {};
-  return { req, res, principal };
+    : null;
+  return { req, res, principal } as Context;
 };
