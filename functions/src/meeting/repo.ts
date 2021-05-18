c3d60add2229eff841853012e9aac9dc646c595f
Bryan Lee <bryanmylee@gmail.com>
2021-05-18 14:54:06 +0800

    feat: handle slug collisions

--- functions/src/meeting/repo.ts
+++ functions/src/meeting/repo.ts
@@ -1,3 +1,4 @@
+import { HttpsError } from 'firebase-functions/lib/providers/https';
 import { Service } from 'typedi';
 import { Repo } from '../firebase/repo';
 import { MeetingEntry } from './types';
@@ -16,6 +17,18 @@ class EditArgs {
 export class MeetingRepo extends Repo<MeetingEntry> {
   constructor() {
     super('meeting');
+  }
+
+  async findBySlug(slug: string) {
+    const results = await this.repo.where('slug', '==', slug).get();
+    if (results.docs.length > 1) {
+      throw new HttpsError('internal', `meeting(slug=${slug}) not unique`);
+    }
+    if (results.docs.length === 0) {
+      throw new HttpsError('not-found', `meeting(slug=${slug}) not found`);
+    }
+    const doc = results.docs[0];
+    return { ...doc.data(), id: doc.id } as MeetingEntry;
   }
 
   async addNew(newMeeting: AddNewArgs) {
--- functions/src/meeting/service.ts
+++ functions/src/meeting/service.ts
@@ -1,6 +1,9 @@
+import { HttpsError } from 'firebase-functions/lib/providers/https';
 import { nanoid } from 'nanoid';
 import { Inject, Service } from 'typedi';
 import { MeetingRepo } from './repo';
+
+const MAX_ATTEMPTS = 5;
 
 class AddNewArgs {
   name: string;
@@ -20,11 +23,26 @@ export class MeetingService {
     return this.repo.findById(id);
   }
 
+  async findBySlug(slug: string) {
+    return this.repo.findBySlug(slug);
+  }
+
+  private async generateSlug() {
+    for (let attempt = 1; attempt <= MAX_ATTEMPTS; attempt++) {
+      const slug = nanoid(12); // ~1000 years before 1% collision at 1000 events per hour
+      try {
+        await this.findBySlug(slug);
+      } catch (error) {
+        if ((error as HttpsError).code === 'not-found') {
+          return slug;
+        }
+      }
+    }
+    throw new HttpsError('internal', 'could not generate new slug');
+  }
+
   async addNew(newMeeting: AddNewArgs) {
-    return this.repo.addNew({
-      ...newMeeting,
-      slug: nanoid(12), // ~1000 years before 1% collision at 1000 events per hour
-    });
+    return this.repo.addNew({ ...newMeeting, slug: await this.generateSlug() });
   }
 
   async edit(id: string, editArgs: EditArgs) {
