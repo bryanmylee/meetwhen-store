b2ab6f10e34542107503b9a5328b3a502d164e0d
Bryan Lee <bryanmylee@gmail.com>
2021-05-24 17:30:57 +0800

    fix: cors on apolloserver

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,5 @@
 import { ApolloServer } from 'apollo-server-express';
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
 import express from 'express';
 import * as functions from 'firebase-functions';
 import 'reflect-metadata';
@@ -13,10 +12,6 @@ import { context } from './security/context';
 import { UserResolver } from './user/resolver';
 
 const configureServer = async () => {
-  const app = express();
-  app.use(cors());
-  app.use(cookieParser());
-
   const schema = await buildSchema({
     authChecker,
     resolvers: [UserResolver, MeetingResolver, ScheduleResolver],
@@ -28,14 +23,24 @@ const configureServer = async () => {
     context,
   });
 
-  apolloServer.applyMiddleware({ app });
+  const app = express();
+
+  app.use(cookieParser());
+
+  apolloServer.applyMiddleware({
+    app,
+    cors: {
+      credentials: true,
+      origin: 'http://localhost:3000',
+    },
+  });
 
   return app;
 };
 
 const server = configureServer();
 
-export const api = functions.region('asia-east2').https.onRequest(async (...args) => {
+export const api = functions.region('asia-east2').https.onRequest(async (req, res) => {
   const app = await server;
-  return app(...args);
+  app(req, res);
 });
